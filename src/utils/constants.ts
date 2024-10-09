b672cf788a12e3b9f687f51806e704c7117d7ba3
Rikas <rikasrkf@gmail.com>
2024-10-09 22:00:58 +0530

    nenv

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,3 @@
-export const API_KEY = "921c7880fe1f496989d23c02d9831c6f";
-export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+import { API_KEY, BASE_URL } from "@env";
+
+export { API_KEY, BASE_URL };
